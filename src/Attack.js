1518c6183718efdfba1287af5a494f882d739bcd
TedCart <TedCart@gmail.com>
2018-04-10 14:56:49 -0400

    Attack.js returns an array when enemy is defeated

--- src/Attack.js
+++ src/Attack.js
@@ -6,9 +6,15 @@ function Attack ({character, enemy}) {
   let healthAfterDamage = enemy.health - attackDamage
   if (healthAfterDamage < 0) healthAfterDamage = 0
 
-  const message = `You hit the ${enemy.name} and did ${(enemy.health - healthAfterDamage)} damage.`
+  let message = `You hit the ${enemy.name} and did ${(enemy.health - healthAfterDamage)} damage.`
 
   enemy.health = healthAfterDamage
+  if (enemy.health === 0) {
+    message = [
+      message,
+      `You defeated the ${enemy.name}!!`
+    ]
+  }
   return ({ enemy, message })
 }
 
