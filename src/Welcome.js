7dd9cc61f8da76e9782239d9f40a5bd164122d01
TedCart <TedCart@gmail.com>
2018-04-12 16:29:25 -0400

    Commented out lines not for welcome function

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,10 +2,10 @@ function Welcome({character, enemy}) {
   const welcomeMessage = [
     `Welcome to Rocking Fighter (patent pending)!!`,
     `Type "help" at any time for.... well, for help.`,
-    `You are in a cave.`,
-    `You see a ${enemy.name}. It is a suspicious ${enemy.name}.`,
-    `You are now battling the ${enemy.name}!`,
-    `What do you do?`
+    // `You are in a cave.`,
+    // `You see a ${enemy.name}. It is a suspicious ${enemy.name}.`,
+    // `You are now battling the ${enemy.name}!`,
+    // `What do you do?`
   ]
 
   return welcomeMessage
